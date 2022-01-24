6453fb68c923a5bc6c0a9d04bfb02f5961fb842c
Your Name <ccu.schaller@outlook.com>
2022-01-24 20:50:49 +0100

    added some logic

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 import spacy
 
 nlp = spacy.load("de_core_news_sm")
-text = 'Spile Russischer Hardbass in meinem Zimmer!'
+text = 'Schalte den Computer aus'
 doc = nlp(text)
-print(doc.ents)
-for entity in doc.ents:
-    print(entity.text, '--- ', entity.label_)
+
+#print(doc.ents)
+#for entity in doc.ents:
+#    print(entity.text, '--- ', entity.label_)
 
 for i in doc:
-    print(i.text, "---", i.pos_, "---", i.dep_,)
+    #print(i.text, "---", i.pos_, "---", i.dep_,)
+    if i.dep_ == 'ROOT':
+        print("Action: ", i.head.text)
+    if i.pos_ == 'NOUN' and not i.dep_ == 'ROOT':
+        if i.dep_ == 'nk':
+           print("Wo: ", i.text)
+        elif i.dep_ == 'sb' or i.dep_ == 'oa':
+            print("Was: ", i.text)
+    if i.pos_ == 'ADV' or i.dep_ == 'svp':
+        print("Wie: ", i.text)
