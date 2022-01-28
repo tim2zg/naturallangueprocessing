932e77284e729683a9d6eefd390d5eabd0315211
Your Name <ccu.schaller@outlook.com>
2022-01-28 12:01:26 +0100

    done some stuff

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import npl_first
-import processing
+import runner
 
 input_text = input("Enter text: ")
 result = npl_first.make_first_nlp(input_text)
 
 print(result)
 
-print(processing.chat(result["Action"]))
+print(runner.run(result["Action"]))
--- processing/__init__.py
+++
@@ -1,5 +0,0 @@
-import runner
-
-
-def chat(message):
-    return runner.run(message)
