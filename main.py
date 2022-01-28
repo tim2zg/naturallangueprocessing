a169a732d7f40672cf8904e07f62c72fd9feab51
Your Name <ccu.schaller@outlook.com>
2022-01-28 13:57:20 +0100

    wow some nice progress :)

--- main.py
+++ main.py
@@ -7,3 +7,6 @@ result = npl_first.make_first_nlp(input_text)
 print(result)
 
 print(runner.run(result["Action"]))
+print(runner.run(result["Wo"]))
+print(runner.run(result["Was"]))
+print(runner.run(result["Wie"]))
